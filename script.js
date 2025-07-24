f316b9a2bef305a3cff75fd4caae2f33ef5127fb
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-24 15:22:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
+function getFormvalue(e) {
+	e.preventDefault();
+	const firstname = document.querySelector("input[name = fname").value.trim();
+	const lastname = document.querySelector("input[name = lname"").value.trim();
+
+	alert(`${firstname} ${lastname}`);
     //Write your code here
 
 }
